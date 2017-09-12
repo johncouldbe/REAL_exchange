c0d9cb7dfecae065d51f611efe1f592b7b0f8018
johncouldbe <johndouglasb@gmail.com>
2017-09-12 18:44:59 -0400

    Removed uneeded socket.io from package.json and changed file upload to ../tmp folder to try and fix heroku bug

--- routes/posts.js
+++ routes/posts.js
@@ -139,7 +139,7 @@ router.post('/image/upload/:postId', isAuthenticated, (req, res) => {
   // specify that we want to allow the user to upload multiple files in a single request
   form.multiples = true;
   // store all uploads in the /uploads directory
-  form.uploadDir = path.join(__dirname, '../uploads');
+  form.uploadDir = path.join(__dirname, '../tmp');
 
   // every time a file has been uploaded successfully,
   // rename it to it's orignal name
--- routes/users.js
+++ routes/users.js
@@ -104,86 +104,76 @@ router.get('/:id', isAuthenticated, function(req, res) {
   })
 });
 
-router.post('/create/newUser', (req, res) => {
-  if (!req.body) {
-    return res.status(400).json({message: 'No request body'});
-  }
-
-  if (!('licenseNumber' in req.body)) {
-    return res.status(422).json({message: 'Missing field: licenseNumber'});
-  }
-
-  let {licenseNumber, password, firstName, lastName, profilePic, bio, phoneNumber, email, website, associations ,friends} = req.body;
-
-  if (typeof licenseNumber !== 'number') {
-    return res.status(422).json({message: 'Incorrect field type: licenseNumber'});
-  }
-
-
-  if (licenseNumber === '') {
-    return res.status(422).json({message: 'Incorrect field length: licenseNumber'});
-  }
-
-  if (!(password)) {
-    return res.status(422).json({message: 'Missing field: password'});
-  }
-
-  if (typeof password !== 'string') {
-    return res.status(422).json({message: 'Incorrect field type: password'});
-  }
-
-  password = password.trim();
-
-  if (password === '') {
-    return res.status(422).json({message: 'Incorrect field length: password'});
-  }
-
-  // check for existing user
-  return User
-    .find({licenseNumber})
-    .count()
-    .exec()
-    .then(count => {
-      if (count > 0) {
-        return res.status(422).json({message: 'licenseNumber already taken'});
-      }
-      // if no existing user, hash password
-      return User.hashPassword(password)
-    })
-    .then(hash => {
-      return User
-        .create({
-          licenseNumber: licenseNumber,
-          password: hash,
-          firstName: firstName,
-          lastName: lastName,
-          profilePic: profilePic,
-          bio: bio,
-          phoneNumber: phoneNumber,
-          email: email,
-          website: website,
-          associations: associations,
-          friends: friends
-        })
-    })
-    .then(user => {
-      return res.status(201).json(user.apiRepr());
-    })
-    .catch(err => {
-      res.status(500).json({message: 'Internal server error'})
-    });
-});
-
-// app.put('/users/:id', (req, res) => {
-//   // ensure that the id in the request path and the one in request body match
-//   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-//     const message = (
-//       `Request path id (${req.params.id}) and request body id ` +
-//       `(${req.body.id}) must match`);
-//     console.error(message);
-//     res.status(400).json({message: message});
+// router.post('/create/newUser', (req, res) => {
+//   if (!req.body) {
+//     return res.status(400).json({message: 'No request body'});
 //   }
+//
+//   if (!('licenseNumber' in req.body)) {
+//     return res.status(422).json({message: 'Missing field: licenseNumber'});
+//   }
+//
+//   let {licenseNumber, password, firstName, lastName, profilePic, bio, phoneNumber, email, website, associations ,friends} = req.body;
+//
+//   if (typeof licenseNumber !== 'number') {
+//     return res.status(422).json({message: 'Incorrect field type: licenseNumber'});
+//   }
+//
+//
+//   if (licenseNumber === '') {
+//     return res.status(422).json({message: 'Incorrect field length: licenseNumber'});
+//   }
+//
+//   if (!(password)) {
+//     return res.status(422).json({message: 'Missing field: password'});
+//   }
+//
+//   if (typeof password !== 'string') {
+//     return res.status(422).json({message: 'Incorrect field type: password'});
+//   }
+//
+//   password = password.trim();
+//
+//   if (password === '') {
+//     return res.status(422).json({message: 'Incorrect field length: password'});
+//   }
+//
+//   // check for existing user
+//   return User
+//     .find({licenseNumber})
+//     .count()
+//     .exec()
+//     .then(count => {
+//       if (count > 0) {
+//         return res.status(422).json({message: 'licenseNumber already taken'});
+//       }
+//       // if no existing user, hash password
+//       return User.hashPassword(password)
+//     })
+//     .then(hash => {
+//       return User
+//         .create({
+//           licenseNumber: licenseNumber,
+//           password: hash,
+//           firstName: firstName,
+//           lastName: lastName,
+//           profilePic: profilePic,
+//           bio: bio,
+//           phoneNumber: phoneNumber,
+//           email: email,
+//           website: website,
+//           associations: associations,
+//           friends: friends
+//         })
+//     })
+//     .then(user => {
+//       return res.status(201).json(user.apiRepr());
+//     })
+//     .catch(err => {
+//       res.status(500).json({message: 'Internal server error'})
+//     });
 // });
+
 
 function isAuthenticated (req,res,next) {
    if(req.user){
